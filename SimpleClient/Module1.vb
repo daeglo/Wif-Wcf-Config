e90ff6bf265e7ddea33f52c6200864d6f8ac7682
DaeGlo <daeglo@daeglo.com>
2014-10-14 23:58:02 -0600

    Created a simple WCF service and client

---
+++ SimpleClient/Module1.vb
@@ -0,0 +1,12 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim svc As New SimpleService.ServiceClient()
+
+        Dim result = svc.SayHello()
+
+        Console.WriteLine(result)
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ SimpleService/Service.svc.vb
@@ -0,0 +1,10 @@
+﻿' NOTE: You can use the "Rename" command on the context menu to change the class name "Service" in code, svc and config file together.
+' NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.vb at the Solution Explorer and start debugging.
+Public Class Service
+    Implements IService
+
+    Public Function SayHello() As String Implements IService.SayHello
+        Return "Hello, world!"
+    End Function
+
+End Class
